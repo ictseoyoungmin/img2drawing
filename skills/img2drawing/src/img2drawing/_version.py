66decf9e85a7ce36d1d0b9cae00ec6d11aeb9ba2
ictseoyoungmin <90033184+ictseoyoungmin@users.noreply.github.com>
2026-09-13 02:24:06 +0900

    release: declare 1.0.3rc2 gesture runtime alignment

--- skills/img2drawing/src/img2drawing/_version.py
+++ skills/img2drawing/src/img2drawing/_version.py
@@ -1,6 +1,6 @@
-__version__ = "1.0.3rc1"
-RELEASE_REVISION = "A11"
-RELEASE_SLICE = "v1.0.3rc1_renderer_v10_markmaking_rc"
+__version__ = "1.0.3rc2"
+RELEASE_REVISION = "A12"
+RELEASE_SLICE = "v1.0.3rc2_gesture_runtime_alignment"
 
 _BASE_VERSION = __version__.split("rc")[0]
 PUBLIC_API = f"DrawingSession/{_BASE_VERSION}-vnext"
