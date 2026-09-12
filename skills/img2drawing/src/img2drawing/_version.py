ecd3a049c36fe38a54d208fb51f053130abf6bc2
ictseoyoungmin <90033184+ictseoyoungmin@users.noreply.github.com>
2026-09-13 01:07:20 +0900

    Declare img2drawing 1.0.3rc1 package identity

--- skills/img2drawing/src/img2drawing/_version.py
+++ skills/img2drawing/src/img2drawing/_version.py
@@ -1,6 +1,6 @@
-__version__ = "1.0.2"
-RELEASE_REVISION = "A10"
-RELEASE_SLICE = "v1.0.2_local_first_exact_timelapse"
+__version__ = "1.0.3rc1"
+RELEASE_REVISION = "A11"
+RELEASE_SLICE = "v1.0.3rc1_renderer_v10_markmaking_rc"
 
 _BASE_VERSION = __version__.split("rc")[0]
 PUBLIC_API = f"DrawingSession/{_BASE_VERSION}-vnext"
